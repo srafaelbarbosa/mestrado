1a01c6424e334ec665a8e9c0177c049744190b03
Rafael Santos <s.rafaelbarbosa@gmail.com>
2019-03-22 09:49:44 -0300

    ajuste da ordem das cores no grafico de gauge

--- resources/views/innovation/result.blade.php
+++ resources/views/innovation/result.blade.php
@@ -97,24 +97,27 @@
 
         var range0 = axis.axisRanges.create();
         range0.value = 0;
-        range0.endValue = 50;
+        range0.endValue = 40;
         range0.axisFill.fillOpacity = 1;
-        range0.axisFill.fill = colorSet.getIndex(0);
+        range0.axisFill.fill = colorSet.getIndex(4);
         range0.axisFill.zIndex = - 1;
 
         var range1 = axis.axisRanges.create();
-        range1.value = 50;
-        range1.endValue = 80;
+        range1.value = 40;
+        range1.endValue = 70;
         range1.axisFill.fillOpacity = 1;
         range1.axisFill.fill = colorSet.getIndex(2);
         range1.axisFill.zIndex = -1;
 
         var range2 = axis.axisRanges.create();
-        range2.value = 80;
+        range2.value = 70;
         range2.endValue = 100;
         range2.axisFill.fillOpacity = 1;
-        range2.axisFill.fill = colorSet.getIndex(4);
+        range2.axisFill.fill = colorSet.getIndex(0);
         range2.axisFill.zIndex = -1;
+
+
+
 
         var hand = chart.hands.push(new am4charts.ClockHand());
 
