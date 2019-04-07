dfa377e7acdb1aeb5cf6c56a7776557ea1b5874e
Rafael Santos <s.rafaelbarbosa@gmail.com>
2019-04-06 23:03:37 -0300

    ajustes de texto

--- resources/views/quiz/evaluate.blade.php
+++ resources/views/quiz/evaluate.blade.php
@@ -58,7 +58,7 @@
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
             <div class="post-heading">
-              <h1 align="center">Avaliação de Softwares Educacionais pelos Indicadores de Inovação</h1>
+              <h1 align="center">Avaliação de Softwares de Suporte à Educação pelos Indicadores de Inovação</h1>
             </div>
           </div>
         </div>
